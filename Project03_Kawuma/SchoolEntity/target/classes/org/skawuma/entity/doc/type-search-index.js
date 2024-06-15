dc93d0442ec3f6e60485913fb838b7ac22c3df25
skawuma <82976186+skawuma@users.noreply.github.com>
2024-06-15 09:49:18 -0400

    slight changes to DbNavigator

---
+++ Project03_Kawuma/SchoolEntity/target/classes/org/skawuma/entity/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.skawuma.entity","l":"Course"},{"p":"org.skawuma.entity","l":"Enrollment"},{"p":"org.skawuma.entity","l":"Student"}];updateSearchResults();
